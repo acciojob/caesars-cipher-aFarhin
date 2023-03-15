f9e03bb2d56017ea9477e6642ddecb27504e1088
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-15 12:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your Script here.
+function rot13(str) {
+  var decodedStr = "";
+  for (var i = 0; i < str.length; i++) {
+    var asciiCode = str.charCodeAt(i);
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      // for uppercase alphabets only
+      var shiftedAsciiCode = (((asciiCode - 65) + 13) % 26) + 65;
+      decodedStr += String.fromCharCode(shiftedAsciiCode);
+    } else {
+      // for non-alphabetic characters
+      decodedStr += str[i];
+    }
+  }
+  return decodedStr;
+}
+
